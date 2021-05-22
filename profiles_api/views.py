174c1f9ff10ee065fe975f00c85c191caa165334
carpmatias <cardenasmatias.1990@gmail.com>
2021-05-22 16:15:29 +0200

    Adding first GET - APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+"""
+
+@author: Matías Cárdenas
+
+"""
+
+# As class based view.
+class HelloApiView(APIView):
+	""" Testing my first APIView"""
+
+	def get(self, request: HttpRequest, format=None) -> HttpResponse:
+		"""Returns a list of APIView features"""
+		an_apiview = [
+			"Uses HTTP methods as function (get, post, patch, put, delete)",
+			"Is similar to a traditional DjangoView",
+			"Gives you the most control over you application logic",
+			"It's mapped manually to URLs",
+		]
+
+		response = {
+			"message": "hello",
+			"an_apiview": an_apiview
+		}
+
+		return Response(response)
