895555cf805082104468c9e977722afcc7e221b7
carpmatias <cardenasmatias.1990@gmail.com>
2021-05-22 17:28:44 +0200

    Adding POST, PUT, PATCH and DELETE methods to first APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,14 @@
+from typing import Optional, Any
+
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+# List of handy http status codes to return
+from rest_framework import status
+
+from profiles_api import serializers
 
 """
 
@@ -12,6 +19,7 @@ from rest_framework.response import Response
 # As class based view.
 class HelloApiView(APIView):
 	""" Testing my first APIView"""
+	serializer_class = serializers.HelloSerializer
 
 	def get(self, request: HttpRequest, format=None) -> HttpResponse:
 		"""Returns a list of APIView features"""
@@ -28,3 +36,43 @@ class HelloApiView(APIView):
 		}
 
 		return Response(response)
+
+	def post(self, request: HttpRequest) -> HttpResponse:
+		""" Create a hello message with our name """
+		# Standard way of working with serializers in APIView
+		serializer = self.serializer_class(data=request.data)
+
+		# same as django forms
+		if serializer.is_valid():
+			name = serializer.validated_data.get("name")
+			message = f"Hello {name}"
+			response = {"message": message}
+			return Response(response)
+		else:
+			return Response(
+				serializer.errors,
+				status=status.HTTP_400_BAD_REQUEST
+			)
+
+	def put(self, request: HttpRequest, pk: Optional[Any] = None) -> HttpResponse:
+		""" Handle update of an object (full object) """
+		response = {
+			"method": "PUT"
+		}
+		return Response(response)
+
+	def patch(self, request: HttpRequest, pk: Optional[Any] = None) -> HttpResponse:
+		""" Handle update of an object (specified field) """
+		response = {
+			"method": "PATCH"
+		}
+		return Response(response)
+
+	def delete(self, request: HttpRequest, pk: Optional[Any] = None) -> HttpResponse:
+		""" Handle deleting of an object """
+		response = {
+			"method": "DELETE"
+		}
+		return Response(response)
+
+
